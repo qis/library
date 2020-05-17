4247fdfaf736b5e2f229a96082eaa7029d6892d8
Alexej Harm <alexej.h@xiphos.de>
2020-05-17 02:14:01 +0200

    removed vs project support

--- make.cmd
+++ make.cmd
@@ -1,23 +1,4 @@
 @echo off
-
-if %0 EQU "%~dpnx0" (
-  cmake -G "Visual Studio 16 2019" -A "x64" ^
-    -DCMAKE_CONFIGURATION_TYPES="Debug;Release" ^
-    -DCMAKE_INSTALL_PREFIX="%~dp0build\install" ^
-    -DCMAKE_TOOLCHAIN_FILE="%~dp0res\toolchain.cmake" ^
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE="release" ^
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG="debug" ^
-    -B "build/windows"
-  if %errorlevel% == 0 (
-    for %%f in (build\windows\*.sln) do (
-      %%f
-      goto :eof
-    )
-  )
-  pause
-  goto :eof
-)
-
 if "%__VS_VCVARS64%" == "1" goto :make
 
 set __VS_LOCATION=%ProgramFiles(x86)%\Microsoft Visual Studio\2019
